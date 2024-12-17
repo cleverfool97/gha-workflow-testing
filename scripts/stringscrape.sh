bfdee1f4527a2202fe91afb758b9ee2ef236ab4b
James Wang <jameswang@roblox.com>
2024-12-16 18:07:02 -0800

    Write results to directory and export

--- scripts/stringscrape.sh
+++ scripts/stringscrape.sh
@@ -2,8 +2,8 @@
 # pattern='\"777 ([^"]*)\"(.*)' this is for double quotes
 pattern=''\''777 ([^'\'']*)'\''(.*)' # matches all strings with "'777 " as a prefix
 
+mkdir exports
 # for each file in the parameter
-
 for file in "$@"; do
   echo "---------- $file is being processed ----------"
   # For each line in the file
@@ -11,7 +11,7 @@ for file in "$@"; do
   do
     # Find all strings matching the pattern for the line, and add it to a list
     while [[ $line =~ $pattern ]]; do
-      echo ${BASH_REMATCH[1]} >> scrapedstrings.txt
+      echo ${BASH_REMATCH[1]} >> exports/scrapedstrings.txt
       printf %s\\n "${BASH_REMATCH[1]}"
       line="${BASH_REMATCH[2]}"
     done
